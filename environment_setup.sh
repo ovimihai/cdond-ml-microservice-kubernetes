a32cc01d2ef089da2e1d685b46ffebbc118a32f2
Ovidiu Anicai <ovidiu.anicai@emag.ro>
2022-03-05 12:52:13 +0200

    wip

---
+++ environment_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# environment already has docker and kubernetes installed
+
+sudo yum install python3 python3-pip
+
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
+
+python3 -m venv ~/.devops
+
+source ~/.devops/bin/activate
+
+make install
+
+# https://github.com/hadolint/hadolint/releases/tag/v2.8.0
+
+sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.8.0/hadolint-Linux-x86_64
+
+sudo chmod +x /bin/hadolint
+
